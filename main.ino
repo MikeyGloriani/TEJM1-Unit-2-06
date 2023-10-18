eaf8f0baf1338f59664648e2f753f1e40331af5f
Mikey Gloriani <113134835+MikeyGloriani@users.noreply.github.com>
2023-10-18 09:57:44 -0400

    added main.ino

---
+++ main.ino
@@ -0,0 +1,30 @@
+/* 
+ * Created by: Mikey Gloriani
+ * Created on: Oct 2023
+ *
+ * This is supposed to detect distance using sound 
+ */
+const int trigPin = 10;
+const int echoPin = 9;
+
+float duration, distance;
+
+void setup() {
+  pinMode(trigPin, OUTPUT);
+  pinMode(echoPin, INPUT);
+  Serial.begin(9600);
+}
+
+void loop() {
+  digitalWrite(trigPin, LOW);
+  delayMicroseconds(2);
+  digitalWrite(trigPin, HIGH);
+  delayMicroseconds(10);
+  digitalWrite(trigPin, LOW);
+
+  duration = pulseIn(echoPin, HIGH);
+  distance = (duration*.0343)/2;
+  Serial.print("Distance: ");
+  Serial.println(distance);
+  delay(100);
+}
